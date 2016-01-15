16f8cd56bbc9f72f7eed6ab4c8020d5df1e9a570
dbailleul <diane.bailleul.pro@gmail.com>
2016-01-15 15:11:57 +0100

    modification for long unit tests - 5

--- tests/testthat/test_package_RClone_p1.R
+++ tests/testthat/test_package_RClone_p1.R
@@ -217,4 +217,10 @@ test_that("the sample functions work", {
 	expect_that(sample_loci(posidonia, He = TRUE, haploid = TRUE), throws_error(""))
 	expect_that(sample_units(posidonia, He = TRUE, haploid = TRUE), throws_error(""))
 	
+	if (Sys.getenv("TRAVIS") == "TRUE"  {
+		message("Unit tests too long on Travis")
+	} else {
+		message("Unit tests not too long on Travis")
+	}
+	
 })
