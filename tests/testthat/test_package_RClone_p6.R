4cff12663d2b8d4a9ad26c37e79cc5bb5053979a
dbailleul <diane.bailleul.pro@gmail.com>
2016-05-25 17:38:25 +0200

    unit tests corr still

--- tests/testthat/test_package_RClone_p6.R
+++ tests/testthat/test_package_RClone_p6.R
@@ -29,8 +29,10 @@ test_that("the GenClone function works", {
 	expect_that(res_vp, is_a("data.frame"))
 	expect_that(ncol(res_tot),  equals(24))
  	expect_that(ncol(res_wc),  equals(24))
-	expect_that(sum(is.na(res_tot)),  equals(6))
-	expect_that(sum(is.na(res_wc)),  equals(10))
+	#expect_that(sum(is.na(res_tot)),  equals(6))
+	#expect_that(sum(is.na(res_wc)),  equals(10))
+	expect_that(length(which(res_tot == "NA")),  equals(6))
+	expect_that(length(which(res_wc == "NA")),  equals(10))
 	
 	expect_that(GenClone(posidonia, listMLL = 1),  throws_error(""))
 	expect_that(GenClone(posidonia, vecpop = 1),  throws_error(""))
@@ -67,18 +69,18 @@ test_that("internal functions of GenClone are OK", {
 	#expect_equal(unlist(res_tot[1,16]),  0.0007693264) #autocorrelation case 3 works
 	#expect_equal(unlist(res_tot[1,18]),  0.0008031684) #autocorrelation case 4 works
 	
-	expect_equal(res_tot[1,1], 40) #counts N
+	expect_equal(res_tot[1,1], "40") #counts N
 	expect_equal(res_tot[1,2], "MLG") #detects no MLL
-	expect_equal(res_tot[1,3],  28) #MLG_list works
-	expect_equal(res_tot[1,4],  4.142857, tolerance  = 0.000001) #freq_all works
-	expect_equal(res_tot[1,6],  0.05076926, tolerance  = 0.0000001) #Fis works
-	expect_equal(res_tot[1,8],  0.02568129, tolerance  = 0.0000001) #double-test Fis
-	expect_equal(res_tot[1,10],  0.6923077) #clonal_index works
-	expect_equal(res_tot[1,24],  33.91304, tolerance  = 0.00001) #double-test clonal_index
-	expect_equal(res_tot[1,11],  1.180756, tolerance  = 0.0000001) #Pareto_index works
-	expect_equal(res_tot[1,12],  0.001230855, tolerance  = 0.0000001) #autocorrelation case 1 works
-	expect_equal(res_tot[1,14],  0.0012436, tolerance  = 0.00001) #autocorrelation case 2 works
-	expect_equal(res_tot[1,16],  0.0007693264) #autocorrelation case 3 works
-	expect_equal(res_tot[1,18],  0.0008031684) #autocorrelation case 4 works
+	expect_equal(res_tot[1,3],  "28") #MLG_list works
+	expect_equal(as.numeric(res_tot[1,4]),  4.142857, tolerance  = 0.000001) #freq_all works
+	expect_equal(as.numeric(res_tot[1,6]),  0.05076926, tolerance  = 0.0000001) #Fis works
+	expect_equal(as.numeric(res_tot[1,8]),  0.02568129, tolerance  = 0.0000001) #double-test Fis
+	expect_equal(as.numeric(res_tot[1,10]),  0.6923077) #clonal_index works
+	expect_equal(as.numeric(res_tot[1,24]),  33.91304, tolerance  = 0.00001) #double-test clonal_index
+	expect_equal(as.numeric(res_tot[1,11]),  1.180756, tolerance  = 0.0000001) #Pareto_index works
+	expect_equal(as.numeric(res_tot[1,12]),  0.001230855, tolerance  = 0.0000001) #autocorrelation case 1 works
+	expect_equal(as.numeric(res_tot[1,14]),  0.0012436, tolerance  = 0.00001) #autocorrelation case 2 works
+	expect_equal(as.numeric(res_tot[1,16]),  0.0007693264) #autocorrelation case 3 works
+	expect_equal(as.numeric(res_tot[1,18]),  0.0008031684) #autocorrelation case 4 works
 
 })
